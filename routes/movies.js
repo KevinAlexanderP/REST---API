5293e97bc6f3b06c0459595337c5b14413eb18f5
KevinAlexanderP <kennedyisdead13@gmail.com>
2020-07-24 21:39:56 -0500

    adjustments on post

--- routes/movies.js
+++ routes/movies.js
@@ -11,15 +11,49 @@ res.send(moviesdb)
 // router.post('/'())
 
 router.post('/',(req,res)=>{
-    const{title, director, year , rating }= (req.body)
-    if(title && director && year && rating){
-        
-        res.json('saved')
+    const id = moviesdb.length + 1;
+    const{title, Director, Year }= req.body;
+    const newMoview = {...req.body, id };
+    if(id && title && Director && Year ){
+        moviesdb.push(newMoview)
+        res.json(moviesdb)
     }else{
-        res.send('received')
+      res.send('Wrong')
     }
     
     });
+
+    
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, director, year, rating } = req.body;
+    if (id && title && director && year && rating) {
+        _.each(movies, (movie, i) => {
+            if (movie.id === id) {
+                movie.title = title;
+                movie.director = director;
+                movie.year = year;
+                movie.rating = rating;
+            }
+        });
+        res.json(movies);
+    } else {
+        res.status(500).json({error: 'There was an error.'});
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    if (id) {
+        _.each(movies, (movie, i) => {
+            if (movie.id == id) {
+                movies.splice(i, 1);
+            }
+        });
+        res.json(movies);
+    }
+});
+
 
 
 module.exports = router;
