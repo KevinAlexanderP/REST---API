0d5cefa42c457ab98ca7094906175c3c479c947a
KevinAlexanderP <kennedyisdead13@gmail.com>
2020-07-25 10:46:07 -0500

    commit

--- routes/movies.js
+++ routes/movies.js
@@ -25,28 +25,38 @@ router.post('/',(req,res)=>{
     });
 
     
-// router.put('/:id', (req, res) => {
-//     const { id } = req.params;
-//     const { title, director, year, rating } = req.body;
-//     if (id && title && director && year && rating) {
-//         _.each(movies, (movie, i) => {
-//             if (movie.id === id) {
-//                 movie.title = title;
-//                 movie.director = director;
-//                 movie.year = year;
-//                 movie.rating = rating;
-//             }
-//         });
-//         res.json(movies);
-//     } else {
-//         res.status(500).json({error: 'There was an error.'});
-//     }
-// });
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, Director, Year} = req.body;
+    if (id && title && director && year && rating) {
+        _.each(movies, (movie, i) => {
+            if (movie.id === id) {
+                movie.title = title;
+                movie.director = director;
+                movie.year = year;
+                movie.rating = rating;
+            }
+        });
+        res.json(movies);
+    } else {
+        res.status(500).json({error: 'There was an error.'});
+    }
+});
+
 
 router.delete('/:id', (req, res) => {
-console.log(req.params)
-res.send('deleted')
-});
+    const {id} = req.params;
+    if(id){
+        _.each(movies,(movie,i)=>{
+            if (movie.id == id ){
+                movies.splice(i,1)
+            }
+        })
+      res.json(moviesdb)
+    }
+})
+
+
 
 
 
